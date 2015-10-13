70e19526e4d4f59309c33b46526addd7616ccb9b
Theodeathne <fafane65@hotmail.com>
2015-10-13 23:35:47 +0200

    Rename folder & separate grabber

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # vision
+
+Visp :
+- Distorsion camera
+- Calibration camera
+- Line tracking
+- Blob tracing
+- Ellipse tracking
+- Model based tracking
+
+OpenCV :
+- Extracton Bag of word with SIFT
+- Matching by SIFT
+
+Images :
+- Set of calibration image
---
+++ visp/Dev-Visp/tutorial-grabber-opencv.cpp
@@ -0,0 +1,29 @@
+/*! \example tutorial-grabber-opencv.cpp */
+#include <visp/vpDisplayOpenCV.h>
+#include <visp/vpOpenCVGrabber.h>
+
+int main()
+{
+#if defined(VISP_HAVE_OPENCV) && (VISP_HAVE_OPENCV_VERSION < 0x020408)
+  try {
+    vpImage<unsigned char> I;
+
+    vpOpenCVGrabber g;
+    g.open(I);
+
+    std::cout << "Image size: " << I.getWidth() << " " << I.getHeight() << std::endl;
+    vpDisplayOpenCV d(I);
+
+    while(1) {
+      g.acquire(I);
+      vpDisplay::display(I);
+      vpDisplay::flush(I);
+      if (vpDisplay::getClick(I, false))
+        break;
+    }
+  }
+  catch(vpException e) {
+    std::cout << "Catch an exception: " << e << std::endl;
+  }
+#endif
+}
